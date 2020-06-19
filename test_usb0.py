f940502b9a8214096fb4d66cab8bb6b1a119c15c
Robert Choi <robertchoimail@gmail.com>
2020-06-19 11:53:17 +0900

    add mode

--- test_usb0.py
+++ test_usb0.py
@@ -4,6 +4,7 @@ import serial
 instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
 
 instrument.serial.baudrate = 9600
+instrument.mode = minimalmodbus.MODE_RTU
 
 temperature = instrument.read_register(6, 1)
 print(temperature)
--- test_usb1.py
+++ test_usb1.py
@@ -4,6 +4,7 @@ import serial
 instrument = minimalmodbus.Instrument('/dev/ttyUSB1', 1)
 
 instrument.serial.baudrate = 9600
+instrument.mode = minimalmodbus.MODE_RTU
 
 temperature = instrument.read_register(6, 1)
 print(temperature)
